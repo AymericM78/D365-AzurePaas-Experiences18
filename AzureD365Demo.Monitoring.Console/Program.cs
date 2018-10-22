4c3ade1055dc08521aad9cc8b4cd53fd9e404d69
Patrick Payet <payetpatrick@hotmail.fr>
2018-10-22 22:47:54 +0200

    Adding Monitoring console #CRM side

---
+++ AzureD365Demo.Monitoring.Console/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace AzureD365Demo.Monitoring.Console
+{
+    class Program
+    {
+        private static int expectedCount = 0;
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                var rawExpectedCount = args[0];
+                expectedCount = int.Parse(rawExpectedCount);
+            }
+            
+            System.Console.WriteLine("Press any button to exit");
+            var bg = InitializeBackgroundWorker();
+            DrawTextProgressBar("Contacts dans mon CRM", 0, expectedCount);
+            bg.RunWorkerAsync();
+            System.Console.ReadKey();
+        }
+
+        public static QueryExpression GetContacts()
+        {
+            return new QueryExpression("contact")
+            {
+                ColumnSet = new ColumnSet("contactid"),
+                NoLock = true
+            };
+        }
+
+        public static BackgroundWorker InitializeBackgroundWorker()
+        {
+            BackgroundWorker bg = new BackgroundWorker();
+            bg.DoWork += Bg_DoWork;
+            bg.ProgressChanged += Bg_ProgressChanged;
+            bg.WorkerReportsProgress = true;
+            return bg;
+        }
+
+        //Fake
+        private static int Count;
+
+        private static void CoreCount(BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            var count = RetrieveMultipleHelper.RetrieveMultipleAllPages(GetContacts());
+            Count += count.Entities.Count;
+            worker.ReportProgress(Count);
+        }
+
+        private static void Bg_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            DrawTextProgressBar("Contacts dans mon CRM", e.ProgressPercentage, expectedCount);
+        }
+
+        private static void Bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                CoreCount((BackgroundWorker) sender, e);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        public static void DrawTextProgressBar(string stepDescription, int progress, int total)
+        {
+            int totalChunks = 30;
+
+            //draw empty progress bar
+            System.Console.CursorLeft = 0;
+            System.Console.Write("["); //start
+            System.Console.CursorLeft = totalChunks + 1;
+            System.Console.Write("]"); //end
+            System.Console.CursorLeft = 1;
+
+            double pctComplete = Convert.ToDouble(progress) / total;
+            int numChunksComplete = Convert.ToInt16(totalChunks * pctComplete);
+
+            //draw completed chunks
+            System.Console.BackgroundColor = ConsoleColor.Green;
+            System.Console.Write("".PadRight(numChunksComplete));
+
+            //draw incomplete chunks
+            System.Console.BackgroundColor = ConsoleColor.Gray;
+            System.Console.Write("".PadRight(totalChunks - numChunksComplete));
+
+            //draw totals
+            System.Console.CursorLeft = totalChunks + 5;
+            System.Console.BackgroundColor = ConsoleColor.Black;
+
+            string output = progress.ToString() + " of " + total.ToString();
+            System.Console.Write(output.PadRight(15) + stepDescription); //pad the output so when changing from 3 to 4 digits we avoid text shifting
+        }
+    }
+}
---
+++ AzureD365Demo.Monitoring.Console/RetrieveMultipleHelper.cs
@@ -0,0 +1,66 @@
+﻿using System.Configuration;
+using System.Net;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace AzureD365Demo.Monitoring.Console
+{
+    
+    public static class RetrieveMultipleHelper
+    {
+        private static IOrganizationService GetProxy()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new CrmServiceClient(ConfigurationManager.ConnectionStrings["Xrm"].ConnectionString);
+            return client.OrganizationServiceProxy;
+        }
+
+        public static EntityCollection RetrieveMultipleAllPages(QueryExpression pagequery)
+        {
+            // Query using the paging cookie.
+            // Define the paging attributes.
+            // The number of records per page to retrieve.
+            int queryCount = 5000;
+
+            // Initialize the page number.
+            int pageNumber = 1;
+
+            // Create the query expression and add condition.
+            // Assign the pageinfo properties to the query expression.
+            pagequery.PageInfo = new PagingInfo {Count = queryCount, PageNumber = pageNumber, PagingCookie = null};
+
+            // The current paging cookie. When retrieving the first page, 
+            // pagingCookie should be null.
+            //Retrieving records in pages...
+            EntityCollection results = new EntityCollection();
+
+            IOrganizationService crmService = GetProxy();
+            while (true)
+            {
+                // Retrieve the page.                   
+                var result = crmService.RetrieveMultiple(pagequery);
+                results.Entities.AddRange(result.Entities);
+
+
+                // Check for more records, if it returns true.
+                if (result.MoreRecords)
+                {
+                    // Increment the page number to retrieve the next page.
+                    pagequery.PageInfo.PageNumber++;
+
+                    // Set the paging cookie to the paging cookie returned from current results.
+                    pagequery.PageInfo.PagingCookie = result.PagingCookie;
+
+                }
+                else
+                {
+                    // If no more records are in the result nodes, exit the loop.
+                    break;
+                }
+            }
+            return results;
+        }
+    }
+}
